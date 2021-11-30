06a25c8a45af9ed27fdf8200c3b5a6c3f816edf3
Caroline crepin <crepin.caroline@gmail.com>
2021-11-30 22:18:48 +0100

    quete 4

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,15 +1,19 @@
 <?php
-
+// src/Controller/ProgramController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+* @Route("/program", name="program_")
+*/
+
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program", name="program_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
